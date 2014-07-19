be33dceb17f6f2798eaaef02d628d9859d31f761
eweing <baggajin@Gadori-MacBook-Pro.local>
2014-07-19 12:20:40 +0900

    delete temp
    파일 정

--- westudy/app/src/main/java/com/example/godong/westudy/MainActivity.java
+++ westudy/app/src/main/java/com/example/godong/westudy/MainActivity.java
@@ -154,7 +154,7 @@ public class MainActivity extends FragmentActivity
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-            View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
 
